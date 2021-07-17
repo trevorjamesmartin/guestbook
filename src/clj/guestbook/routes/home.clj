20f0636bd97477cf1132d0cba0ed9307e530b51c
Trevor Martin <trevor.j.martin@gmail.com>
2021-07-17 11:19:18 -0400

    separate model from web-server

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -1,22 +1,24 @@
 (ns guestbook.routes.home
   (:require
    [guestbook.layout :as layout]
-   [guestbook.db.core :as db]
-   [clojure.java.io :as io]
+   [guestbook.messages :as msg]
    [guestbook.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]
-   [guestbook.validation :refer [validate-message]]))
+   [ring.util.http-response :as response]))
 
 (defn save-message! [{:keys [params]}]
-  (if-let [errors (validate-message params)]
-    (response/bad-request {:errors errors})
-    (try
-      (db/save-message! params)
-      (response/ok {:status :ok})
-      (catch Exception e
-        (response/internal-server-error
-         {:errors {:server-error ["Failed to save message!"]}})))))
+  (try
+    (msg/save-message! params)
+    (response/ok {:status :ok})
+    (catch Exception e
+      (let [{id :guestbook/error-id
+             errors :errors} (ex-data e)]
+        (case id
+          :validation
+          (response/bad-request {:errors errors})
+          ;;else
+          (response/internal-server-error
+           {:errors {:server-error ["Failed to save message!"]}}))))))
 
 (defn home-page [request]
   (layout/render
@@ -27,7 +29,7 @@
   (layout/render request "about.html"))
 
 (defn message-list [_]
-  (response/ok {:messages (vec (db/get-messages))}))
+  (response/ok (msg/message-list)))
 
 (defn home-routes []
   [""
