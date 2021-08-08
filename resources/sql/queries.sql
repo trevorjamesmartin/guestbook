addb44edd1ed218c330001b4791c729c7c48a3d4
Trevor Martin <trevor.j.martin@gmail.com>
2021-08-08 19:38:07 -0400

    improve posts

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -7,15 +7,17 @@ RETURNING *;
 
 -- :name get-messages :? :*
 -- :doc selects all available messages
-SELECT
-  p.id              as id,
-  p.timestamp       as timestamp,
-  p.message         as message,
-  p.name            as name,
-  p.author          as author,
-  a.profile->>'avatar' as avatar
-FROM posts AS p JOIN users AS a
-ON a.login = p.author
+SELECT * FROM posts_with_meta
+
+-- :name get-message :? :1
+-- :doc selects a message
+SELECT * FROM posts_with_meta
+WHERE id = :id
+
+-- :name get-messages-by-author :? :*
+-- :doc selects all messages posted by a user
+SELECT * FROM posts_with_meta
+WHERE author = :author
 
 -- :name create-user!* :! :n
 -- :doc creates a new user with the provided login and hashed password
@@ -77,15 +79,71 @@ WHERE login = :login
 DELETE FROM users
 WHERE login = :login
 
--- :name get-message :? :1
--- :doc selects a message
-SELECT
-  p.id                  as id,
-  p.timestamp           as timestamp,
-  p.message             as message,
-  p.name                as name,
-  p.author              as author,
-  a.profile->>'avatar'  as avatar
-FROM posts AS p JOIN users AS a
-ON a.login = p.author
-WHERE p.id = :id
+-- :name boost-post! :! :n
+-- :doc Boosts a post, or moves a boost to the top of the user's timeline
+INSERT INTO boosts
+(user_id, post_id, poster)
+VALUES (:user, :post, nullif(:poster, :user))
+ON CONFLICT (user_id, post_id) DO UPDATE
+SET timestamp = now()
+WHERE boosts.user_id = :user
+AND   boosts.post_id = :post
+
+-- :name boosters-of-post :? :*
+-- :doc Get all boosters of a post
+SELECT user_id as user FROM boosts
+WHERE post_id = :post
+
+-- :name get-reboosts :? :*
+-- :doc Get all boosts descended from a given boost
+WITH RECURSIVE reboosts AS
+(WITH post_boosts AS
+  (SELECT user_id, poster
+    FROM boosts
+    WHERE post_id = :post)    
+    SELECT user_id, poster
+    FROM post_boosts
+    WHERE user_id = :user
+    UNION
+    SELECT b.user_id, b.poster
+    FROM post_boosts b INNER JOIN reboosts r ON r.user_id = b.poster)
+SELECT user_id as user, poster as source from reboosts
+
+-- :name get-boost-chain :? :*
+-- :doc Gets all boosts above the original boost
+WITH RECURSIVE reboosts AS
+(WITH post_boosts AS
+  (SELECT user_id, poster
+  FROM boosts
+  WHERE post_id = :post)
+  SELECT user_id, poster
+  FROM post_boosts
+  WHERE user_id = :user
+  UNION
+  SELECT b.user_id, b.poster
+  FROM post_boosts b INNER JOIN reboosts r on r.poster = b.user_id)
+SELECT user_id AS user, poster AS source FROM reboosts
+
+-- :name get-timeline :? :*
+-- :doc Gets the latest post or boost for each post
+SELECT * FROM
+(SELECT DISTINCT ON (p.id) * FROM posts_and_boosts as p
+  ORDER BY p.id, p.posted_at desc) AS t
+ORDER BY t.posted_at asc
+
+-- :name get-timeline-for-poster :? :*
+-- :doc Gets the latest post or boost for each post
+SELECT * FROM
+(SELECT DISTINCT ON (p.id) * FROM posts_and_boosts as p
+  WHERE p.poster = :poster
+  ORDER BY p.id, p.posted_at desc) AS t
+ORDER BY t.posted_at asc
+
+-- :name get-timeline-post :? :1
+-- :doc Gets the boosted post for updating timelines
+SELECT * FROM posts_and_boosts
+WHERE is_boost = :is_boost
+AND poster = :user
+AND id = :post
+ORDER BY posted_at asc
+LIMIT 1
